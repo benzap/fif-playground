575e66fd6dd27b428d50da877698dd9ea36dba4b
Benjamin Zaporzan <benzaporzan@gmail.com>
2018-04-21 01:41:48 -0400

    Fixed urls for relative paths

--- src/fif_pg/components/dropdown.cljs
+++ src/fif_pg/components/dropdown.cljs
@@ -25,18 +25,18 @@
   (let [{:keys [dropdown-open?]} (rum/react app-state)]
     [:select.editor-dropdown {:on-change #(set-editor-example! app-state (-> % .-target .-value))}
      [:option {:value ""} ""]
-     [:option {:value "/examples/basic_arithmetic.fif"} "Basic Arithmetic"]
-     [:option {:value "/examples/conditional_operators.fif"} "Conditional Operators"]
-     [:option {:value "/examples/control_structures.fif"} "Control Structures"]
-     [:option {:value "/examples/creating_function.fif"} "Defining Functions"]
-     [:option {:value "/examples/loop_structures.fif"} "Loop Structures"]
-     [:option {:value "/examples/defining_variables.fif"} "Defining Variables"]
-     [:option {:value "/examples/collection_operators.fif"} "Collection Operators"]
-     [:option {:value "/examples/word_referencing.fif"} "Word Referencing"]
-     [:option {:value "/examples/functional_operators.fif"} "Functional Operators"]
-     [:option {:value "/examples/the_collecter.fif"} "The Collecter"]
-     [:option {:value "/examples/the_realizer.fif"} "The Realizer"]
-     [:option {:value "/examples/macros.fif"} "Macros"]
-     [:option {:value "/examples/query_example.fif"} "Query Example"]]))
+     [:option {:value "examples/basic_arithmetic.fif"} "Basic Arithmetic"]
+     [:option {:value "examples/conditional_operators.fif"} "Conditional Operators"]
+     [:option {:value "examples/control_structures.fif"} "Control Structures"]
+     [:option {:value "examples/creating_function.fif"} "Defining Functions"]
+     [:option {:value "examples/loop_structures.fif"} "Loop Structures"]
+     [:option {:value "examples/defining_variables.fif"} "Defining Variables"]
+     [:option {:value "examples/collection_operators.fif"} "Collection Operators"]
+     [:option {:value "examples/word_referencing.fif"} "Word Referencing"]
+     [:option {:value "examples/functional_operators.fif"} "Functional Operators"]
+     [:option {:value "examples/the_collecter.fif"} "The Collecter"]
+     [:option {:value "examples/the_realizer.fif"} "The Realizer"]
+     [:option {:value "examples/macros.fif"} "Macros"]
+     [:option {:value "examples/query_example.fif"} "Query Example"]]))
 
 
